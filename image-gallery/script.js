0e811edcaf92f43bb075068b972daf20d7425fae
Tanya-Gro <tarpel.gta@gmail.com>
2024-09-30 16:40:51 +0300

    add: search request

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,22 +1,41 @@
-const url = "https://api.unsplash.com/";
-const atribute = "/photos/random?query=fall&client_id=VW99YuRZqLS5w5qmq8V76A6v7e7NnlGFO7-pHJdAoH0";
 const cardBox = document.querySelector('.cardBox');
+const searchInput = document.querySelector('.searchInput');
 const searchButton = document.querySelector('.searchButton');
-const URLs = [];
+const url = "https://api.unsplash.com/";
+let atribute = "/photos/random?query=fall&client_id=VW99YuRZqLS5w5qmq8V76A6v7e7NnlGFO7-pHJdAoH0";
+let URLs = [];
 
-async function getImages() {
-    const res = await fetch(url + atribute);
+async function getImages(link) {
+    const res = await fetch(link);
     const data = await res.json();
     showImage(data);
     // console.log(data);
 }
-for (let i = 0; i < 6; i++)
-    getImages();
+function getStarted() {
+    for (let i = 0; i < 6; i++)
+        getImages(`${url}${atribute}`);
+    // getImages();
+}
+getStarted();
 
 function showImage(data) {
     const img = cardBox.appendChild(document.createElement('div'));
     img.setAttribute('style', `background-image: url("${data.urls.small}")`);
     img.classList.add('card');
     URLs.push(data.urls.regular);
-    // cardBox.firstChild.setAttribute('src', data.urls.regular);
 }
+
+function deletePicktures() {
+    URLs.length = 0;
+    const cards = document.querySelectorAll('.card');
+    for (let i = 0; i < cards.length; i++)
+        cards[i].remove();
+};
+
+searchButton.addEventListener('click', () => {
+    let oldItem = atribute.slice(atribute.indexOf('=') + 1, atribute.indexOf('&'));
+    let newItem = searchInput.value;
+    atribute = atribute.replace(oldItem, newItem);
+    deletePicktures();
+    getStarted();
+});
