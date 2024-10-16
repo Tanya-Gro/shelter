45b79fcd6a699272afffe9ab2c2b2ee02ba2b33c
Tanya-Gro <tarpel.gta@gmail.com>
2024-10-17 00:33:14 +0300

    add: speed up, won, loose

--- random-game/script.js
+++ random-game/script.js
@@ -2,10 +2,10 @@ const cageWidth = 32;
 const widthField = 608;
 let score = 0;
 let way = '';
-let timeSet = 500;
+let timeSet = 1000;
 let preyPosition = new Object;
 let rotateSnakeHead = 0;
-const goal = 5;
+const goal = 6;
 const preyPath = ['./assets/frog.png', './assets/fly.png', './assets/bee.png', './assets/mouse.png'];
 let preyIndex = 0;
 
@@ -18,7 +18,7 @@ const field = new Image();
 field.src = './assets/field.png'
 
 const prey = new Image();
-prey.src = './assets/frog.png';
+prey.src = preyPath[preyIndex];
 
 function newPreyPosition() {
   preyPosition = {
@@ -31,20 +31,30 @@ newPreyPosition();
 const snake = new Image();
 snake.src = './assets/snake.png';
 
-const fire = new Image();
-fire.src = './assets/fire.png';
-
 let snakePosition = [];
 snakePosition[0] = {
   x: Math.floor(Math.random() * 17 + 1) * cageWidth,
   y: Math.floor(Math.random() * 15 + 1) * cageWidth + 64,
 }
 
-function drawField() {
+const fire = new Image();
+fire.src = './assets/fire.png';
+
+function drawGame() {
   c.drawImage(field, 0, 0);
 
-  prey.src = preyPath[preyIndex];
   c.drawImage(prey, preyPosition.x, preyPosition.y);
+
+  if (snakePosition.length > 1) {
+    for (let i = 1; i < snakePosition.length; i++) {
+      c.beginPath()
+      c.arc(snakePosition[i].x + cageWidth / 2, snakePosition[i].y + cageWidth / 2, cageWidth * 0.4, 0, Math.PI * 2, false);
+      c.fillStyle = "#568a34";
+      c.fill();
+      c.strokeStyle = "black";
+      c.stroke();
+    }
+  }
 
   c.save();
   c.translate(snakePosition[0].x + cageWidth / 2, snakePosition[0].y + cageWidth / 2);
@@ -58,29 +68,25 @@ function drawField() {
   }
   c.restore();
 
-  // c.drawImage(snake, snakePosition[0].x, snakePosition[0].y);
-  if (snakePosition.length > 1) {
-    for (let i = 1; i < snakePosition.length; i++) {
-      c.beginPath()
-      c.arc(snakePosition[i].x + cageWidth / 2, snakePosition[i].y + cageWidth / 2, cageWidth * 0.4, 0, Math.PI * 2, false);
-      c.fillStyle = "#568a34";
-      c.fill();
-      c.strokeStyle = "black";
-      c.stroke();
-    }
-  }
-
   c.fillStyle = "black";
   c.font = "45px system-ui"
   c.fillText(score, 70, 50);
-}
 
-function moveSnake() {
   let snakeHeadX = snakePosition[0].x;
   let snakeHeadY = snakePosition[0].y;
 
-  if (snakeHeadX < cageWidth || snakeHeadX > cageWidth * 17 || snakeHeadY < cageWidth * 3 || snakeHeadY > cageWidth * 17)
-    clearInterval(game);
+  if (snakeHeadX < cageWidth || snakeHeadX > cageWidth * 17 || snakeHeadY < cageWidth * 3 || snakeHeadY > cageWidth * 17 || score == goal) {
+    if (score == goal) {
+      c.font = "75px system-ui"
+      c.fillText('WIN', widthField * 0.35, widthField / 2);
+    }
+    else {
+      c.font = "50px system-ui"
+      c.fillText('Try again. Refresh page', widthField * 0.07, widthField * 0.5);
+    }
+  }
+
+  else if (!eatTail(snakePosition)) setTimeout(drawGame, timeSet);
 
   switch (way) {
     case 'left': snakeHeadX -= cageWidth; break;
@@ -92,12 +98,28 @@ function moveSnake() {
   if (preyPosition.x === snakeHeadX && preyPosition.y === snakeHeadY) {
     newPreyPosition();
     score++;
-    way = '';
+    timeSet -= 170;
     preyIndex = preyIndex + 1 < preyPath.length ? preyIndex + 1 : 0;
+    prey.src = preyPath[preyIndex];
   }
   else snakePosition.pop();
 
   snakePosition.unshift({ x: snakeHeadX, y: snakeHeadY });
+}
+
+function eatTail(body) {
+  for (let i = 1; i < body.length; i++)
+    if (body[0].x == body[i].x && body[0].y == body[i].y) {
+      c.save();
+      c.translate(snakePosition[0].x + cageWidth / 2, snakePosition[0].y + cageWidth / 2);
+      c.rotate((rotateSnakeHead + 180) * Math.PI / 180);
+      c.drawImage(fire, -fire.width / 2, -fire.height / 2);
+      c.restore();
+      c.font = "50px system-ui"
+      c.fillText('Try again. Refresh page', widthField * 0.07, widthField * 0.5);
+      return true;
+    }
+  return false;
 }
 
 function pressKey(event) {
@@ -106,28 +128,24 @@ function pressKey(event) {
       if (way != 'right') {
         rotateSnakeHead = 90;
         way = 'left';
-        moveSnake();
       }
       break;
     case 38:
       if (way != 'down') {
         rotateSnakeHead = 180;
         way = 'up';
-        moveSnake();
       }
       break;
     case 39:
       if (way != 'left') {
         rotateSnakeHead = 270;
         way = 'right';
-        moveSnake();
       }
       break;
     case 40:
       if (way != 'up') {
         rotateSnakeHead = 0;
         way = 'down';
-        moveSnake();
       }
       break;
   }
@@ -135,4 +153,4 @@ function pressKey(event) {
 
 document.addEventListener('keydown', pressKey);
 
-let game = setInterval(drawField, timeSet);
+setTimeout(drawGame, timeSet);
